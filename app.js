a1b3c05972ef6558cecbd0097c94a616f1d1ddfd
lucaslima04 <limazanelatolucas@gmail.com>
2025-04-09 14:26:31 -0300

    Projeto inicial

---
+++ app.js
@@ -0,0 +1,100 @@
+particlesJS('particles-js', 
+    {
+    "particles": {
+        "number": {
+        "value": 4000,
+        "density": {
+            "enable": true,
+            "value_area": 10000.721462448409
+        }
+        },
+        "color": {
+        "value": "#fff"
+        },
+        "shape": {
+        "type": "circle",
+        "stroke": {
+            "width": 0.2,
+            "color": "#fff"
+        },
+        },
+        "opacity": {
+        "value":  9,
+        "random": true,
+        "anim": {
+            "enable": true,
+            "speed": 1.136908097221883,
+            "opacity_min": 0.1,
+            "sync": false
+        }
+        },
+        "size": {
+        "value": 2,
+        "random": true,
+        "anim": {
+            "enable": false,
+            "speed": 3,
+            "size_min": 0.5,
+            "sync": false
+        }
+        },
+        "line_linked": {
+        "enable": true,
+        "distance": 80,
+        "color": "#ffffff",
+        "opacity": 0.5451601006942778,
+        "width": 0.5
+        },
+        "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": true,
+        "straight": false,
+        "out_mode": "out",
+        "attract": {
+            "enable": true,
+            "rotateX": 10000,
+            "rotateY": 10000
+        }
+        }
+    },
+    "interactivity": {
+        "detect_on": "body",
+        "events": {
+        "onhover": {
+            "enable": true,
+            "mode": "grab"
+        },
+        "onclick": {
+            "enable": true,
+            "mode": "push"
+        },
+        "resize": true
+        },
+        "modes": {
+        "grab": {
+            "distance": 100,
+            "line_linked": {
+            "opacity": 2
+            }
+        },
+        "bubble": {
+            "distance": 400,
+            "size": 40,
+            "duration": 2,
+            "opacity": 8,
+            "speed": 3
+        },
+        "push": {
+            "particles_nb": 6
+        },
+        }
+    },
+    "retina_detect": true,
+    }
+);
+
+
+/*------------------------------------------------------------------------------------------------------------------*/
+
